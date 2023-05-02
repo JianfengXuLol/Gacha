dec0a901f026dc2ad09bfa14becb39e14b343dd2
JianfengXuLol <Christophpierce@gmail.com>
2023-05-02 13:00:30 -0300

    Finished all the world pages

--- Regions/Sumeru/Sumeru.php
+++ Regions/Sumeru/Sumeru.php
@@ -13,18 +13,107 @@
 			integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
 			crossorigin="anonymous" />
 		<link rel="stylesheet" href="../../Regions.css" />
-
 		<title>Gemshin Impact Page</title>
 	</head>
 
 	<body>
-    <div class="container mt-3 | theme_card">
- 
-        <div class="container-fluid mt-3">   
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-11 text-white">
-                        <h1>Sumeru</h1>
+		<header>
+			<img src="../../Logo/logo3.png" id="logo" alt="" />
+		</header>
+
+		<div class="MainContent | container-fluid">
+			<div class="navigationMenu">
+				<div class="row">
+					<ul class="nav nav-tabs border-bottom-0">
+					<li class="nav-item dropdown">
+								<a class="nav-link" href="http://localhost/Gacha/Main.php#"><span style="color:white">HOME</span></a>
+							</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle">CHARACTERS</a>
+							<ul class="dropdown-menu">
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/limitedEdition_5_Star/Limited_5_star.php"
+										>Limited 5 Star</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/standard_4_star/standard_4_star.php"
+										>4 Star Characters</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/standard_5star/standard_5star.php"
+										>Standard Banner 5 Star</a
+									>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle">THE WORLD</a>
+							<ul class="dropdown-menu">
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Regions/Mondstadt/Mondstadt.php"
+										>Mondstadt</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Regions/Liyue/Liyue.php"
+										>Liyue</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Regions/Inazuma/Inazuma.php"
+										>Inazuma</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Regions/Sumeru/Sumeru.php"
+										>Sumeru</a
+									>
+								</li>
+							</ul>
+						</li>
+
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle">OTHER</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="http://localhost/Gacha/Tutorials/Tutorials.php">Tutorials</a></li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Daily_Routine/DailyRoutine.php"
+										>Resources Tracker</a
+									>
+								</li>
+								<li>
+									<a
+										class="dropdown-item"
+										href="http://localhost/Gacha/Gacha.php#"
+										>Wishing Pool</a
+									>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row | contents">
+					<div class="col-10 | CityDescription">
+          <h1>Sumeru</h1>
                         <br>
                         <p>Sumeru is one of the seven regions in Teyvat. It is the nation that worships Lesser Lord Kusanali, the Dendro Archon.</p>
                         <br>
@@ -32,36 +121,35 @@
                         <br>
                         <p>Access to Sumeru is unlocked after completing the Archon Quest Prologue: Act I - The Outlander Who Caught the Wind.</p>
                         <p>Archon Quest Chapter III: Truth Amongst the Pages of Purana takes place in this region.</p>
-                    </div>
-                    <div class="col-1 text-white">
-                    
-                        <div class="card" style="width:250px;">
-                            <h2 class="card-header">Sumeru</h2>
+                        <p>Sumeru's landscape features both deserts and forests.[4] In the Teyvat Chapter Storyline Preview, Dainsleif likens it to a desert, and Liben mentions that "Sumeru is all rainforest and desert."500 years ago, during the cataclysm, the previous Dendro Archon, Greater Lord Rukkhadevata, was slain and the current God of Wisdom took her place. Under the aegis of the God of Wisdom, the sages of Sumeru drive themselves into hysterics and abandon all that is worldly in their pursuit of esoteric wisdom.</p>
+					</div>
+					<div class="col-2">
+						<div class="card" style="width: 250px">
+            <h2 class="card-header">Sumeru</h2>
                             <img class="card-img-bottom" src="ThemeImage/Sumeru.webp" alt="Theme image" style="width:100%">
                             <div class="card-body">
                             <p class="card-text">Archon: Buer</p>
                             <p class="card-text">Main City: Sumeru City</p>
                             <p class="card-text">Control­ling En­ti­ty: Sumeru Akademiya</p>                      
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-<div id="demo" class="carousel slide" data-bs-ride="carousel">
-        
-  <!-- Indicators/dots -->
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
-  </div>
-  
-  <!-- The slideshow/carousel -->
-  <div class="carousel-inner">
+						</div>
+					</div>
+			</div>
+			<div class="row | contents">
+					<div class="gallery col-7">
+							<div id="demo" class="carousel slide" data-bs-ride="carousel">
+								<!-- Indicators/dots -->
+								<div class="carousel-indicators">
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
+									<button type="button" data-bs-target="#demo" data-bs-slide-to="5"></button>
+								</div>
+								
+								<!-- The slideshow/carousel -->
+								<div class="carousel-inner">
     <div class="carousel-item active">
       <img src="City/city1.jpg" alt="City#1" class="d-block" style="width:100%">
     </div>
@@ -82,22 +170,147 @@
       <img src="City/city6.webp" alt="City#6" class="d-block" style="width:100%"> 
     </div>
   </div>
-  
-  <!-- Left and right controls/icons -->
-  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </button>
-</div>
-    </div>
 
-		
+								<!-- Left and right controls/icons -->
+								<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+								<span class="carousel-control-prev-icon"></span>
+								</button>
+								<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+								<span class="carousel-control-next-icon"></span>
+								</button>
+							</div>
+					</div>
 
+					<div class="imgDescription col-5">
+							<h2 class="titleColor">Sumeru City</h2>
+							<br>
+							<p>Sumeru City is known across Teyvat as the "City of Wisdom."[1] It is under the jurisdiction of the Sumeru Akademiya and is built on top of the Divine Tree. The city features many districts and points of interest, such as the Grand Bazaar, Zubayr Theater, Treasures Street, and more. Located in the middle to top half of the tree is The Akademiya, which serves as the main base of the Sumeru Akademiya. On top of The Akademiya rests the Sanctuary of Surasthana, which is a treetop palace where Lesser Lord Kusanali resides.</p>
+					</div>
+					<hr>
+					<h4>Sub Areas</h4>
+					<hr>
+					<div class="d-flex">
+						<img
+								src="SubAreas/Ardravi_Valley.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+              Ardravi Valley
+							</h4>
+							<p>
+							Ardravi Valley has the biggest port in Sumeru, Port Ormos. Port Ormos is the largest port in Sumeru and serves as a hub for trading and travel. It is located beneath two giant trees, between which a river runs from Sumeru City to the port. Though parts of Port Ormos are under the supervision of the Akademiya, smuggling and other illegal activity is more common at Port Ormos than in Sumeru City. Prices also tend to be lower than in Sumeru City. Most long-term residents in the port are traders and laborers who grew up in the area, but a number of Eremite merceneries from various factions can also be found there. Students often come to the port when academic life gets too stressful, and it is even considered good luck to some.
+							</p>
+						</div>
+					</div>
+					<hr />
+					<div class="d-flex">
+						<img
+								src="SubAreas/Ashavan_Realm.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Ashavan Realm
+							</h4>
+							<p>
+							There are two major areas are located in Ashavan Realm. Apam Woods is a massive rainforest with trees that stretch towards the sky. After completing the World Quest A Prayer for Rain on the Fecund Land, the player will be able to operate the Varuna Contraption and control the rain. Caravan Ribat is a fortified village at the foot of a tall wall that blocks the sandstorms from destroying the Sumeru rainforests. Located at the foot of the Wall of Samiel, Caravan Ribat serves as a thoroughfare for those coming from the forest going to Aaru Village, and vice versa.
+							</p>
+						</div>
+					</div>
+					<hr />
+					<div class="d-flex">
+						<img
+								src="SubAreas/Avidya_Forest.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Avidya Forest
+							</h4>
+							<p>
+							Avidya Forest is a lush, green forest located in eastern Sumeru. It is the main area and most populous section of Sumeru apart from Ardravi Valley. The forest is also home to Sumeru City which houses The Akademiya, and Gandharva Ville, which is the base of the Forest Watchers. The Forest Watchers have guarded the forest for many years in order to ensure the forest's survival. Many unique flora and fauna grow in the Avidya Forest, such as Padisarahs, Nilopata Lotuses or Rukkhashava Mushrooms. Apart from Flowers, there are also different species and breeds of animals found, like Shroomboars. In the forest, you wil also find many, many special breeds of Shroomkin out in the wild. The Avidya Forest is also the first area in Sumeru that the Traveler enters.
+							</p>
+						</div>
+					</div>
+					<hr />
+					<div class="d-flex">
+						<img
+								src="SubAreas/Lokapala_Jungle.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Lokapala Jungle
+							</h4>
+							<p>
+							Lokapala Jungle has three major places: Chatrakam Cave is best known for containing a large number of Fungi, and humans typically do not go inside; Mawtiyima Forest is a forest made of enormous fluorescent mushrooms. Mawtiyima is described as "an energy form created by the ancient dreams and memories of the forest." This energy fights against The Withering to ensure the health of the forest, and even seems to be able to communicate with the Aranara. In an ancient tongue, the name once meant "human death."; The Palace of Alcazarzaray is owned by Dori. It was designed by Kaveh and is considered to be his magnum opus.
+							</p>
+						</div>
+					</div>
+					<hr />
+					<div class="d-flex">
+						<img
+								src="SubAreas/Lost_Nursery.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Lost Nursery
+							</h4>
+							<p>
+              Lost Nursery has the old home of the Aranara for hundreds of years before the cataclysm. It is unlocked by completing the Aranyaka: Part III - Nursery of Lost Dreams quest, For Fruits, Seeds, and Trees.
+							</p>
+						</div>
+					</div>
+					<div class="d-flex">
+						<img
+								src="SubAreas/Vanarana.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Vanarana
+							</h4>
+							<p>
+              There are two different versions of Vanarana: the one that exists in reality, and the dream version where the Aranara live (which is called Mahavanaranapna).
+							</p>
+						</div>
+					</div>
+					<div class="d-flex">
+						<img
+								src="SubAreas/Vissudha_Field.webp"
+								alt=""
+								class="articleImage"
+						/>
+
+						<div class="article">
+							<h4 class="aritcleTitle titleColor">
+							Vissudha Field
+							</h4>
+							<p>
+              It has the first Vasara Tree, and it acts as the seal for the entrance to Old Vanarana.
+							</p>
+						</div>
+					</div>
+					<hr />
+			</div>
+			
+		</div>
 
 		<footer></footer>
-
 
 		<!-- Bootstrap JavaScript Libraries -->
 		<script
@@ -110,10 +323,8 @@
 			integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
 			crossorigin="anonymous"></script>
 
-		<script src="Gacha.js"></script>
+		<!-- <script src="Gacha.js"></script> -->
 
-		<script>
-			
-		</script>
+		<script></script>
 	</body>
 </html>
