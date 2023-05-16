99106a99297dcb3fca4fb9cdf5f7350c9ae01536
JianfengXuLol <Christophpierce@gmail.com>
2023-05-16 16:24:29 -0300

    responsive design day#1 on tutorial page

--- Tutorials/Tutorials.js
+++ Tutorials/Tutorials.js
@@ -2,60 +2,59 @@ var screenSize;
 var collapseMenu = document.querySelector(".navbar-collapse");
 var bodyElement = document.body;
 var MainContent = document.querySelector(".MainContent");
-var card = document.querySelector(".card");
-var imgDescription = document.querySelector(".imgDescription");
-var article = document.querySelectorAll(".article");
-var articleImage = document.querySelectorAll(".articleImage");
+var imageContainer = document.querySelector(".imageContainer");
+var scroll = document.querySelector(".scroll");
+var portalLinks = document.querySelector(".portalLinks");
 
 function responsiveDisplay() {
 	// Get width and height of the window excluding scrollbars
 	screenSize = document.documentElement.clientWidth;
 	bodyElement.style.maxWidth = screenSize.toString() + "px";
+
+	//The following part is for testing purposes
+	document.querySelector(".windowSize").innerHTML =
+		"Window Size: " + screenSize;
 	console.log("body width: " + bodyElement.style.maxWidth);
 	console.log("Screen width: " + screenSize);
 
 	/*large window size****************************************************************************/
-	if (screenSize >= 1750) {
-		imgDescription.style.marginLeft = "5rem";
+	if (screenSize >= 1600) {
+		imageContainer.style.marginLeft = "5rem";
+	} else {
+		imageContainer.style.marginLeft = "0rem";
 	}
 	/*large window size****************************************************************************/
-	if (screenSize <= 1390) {
+	if (screenSize <= 1382) {
 		MainContent.style.width = "100%";
 		MainContent.style.marginLeft = "0rem";
-		imgDescription.style.marginLeft = "0rem";
+		imageContainer.style.marginLeft = "23rem";
 	} else {
 		MainContent.style.width = "85%";
 		MainContent.style.marginLeft = "10rem";
+		imageContainer.style.marginLeft = "0rem";
 	}
 
 	/*medium window size****************************************************************************/
+	if (screenSize <= 980) {
+		imageContainer.style.marginLeft = "0rem";
+	}
+	/*small window size****************************************************************************/
 	if (screenSize <= 750) {
 		collapseMenu.style.marginLeft = "4.5rem";
 	} else {
 		collapseMenu.style.marginLeft = "0rem";
 	}
-	/*small window size****************************************************************************/
-	if (screenSize <= 576) {
-		card.style.width = "380px";
-		card.style.marginBottom = "2rem";
-		card.style.marginRight = "1rem";
-		imgDescription.style.marginTop = "2rem";
-		imgDescription.style.width = "400px";
 
-		article.forEach(function (li) {
-			li.style.marginTop = "2rem";
-		});
-
-		articleImage.forEach(function (li) {
-			li.style.height = "23vh";
-		});
+	if (screenSize <= 615) {
+		scroll.style.width = "400px";
+		portalLinks.style.fontSize = "0.7rem";
+		portalLinks.style.left = "3rem";
+		portalLinks.style.top = "8rem";
 	} else {
-		card.style.width = "250px";
-		card.style.marginRight = "0rem";
-		article.forEach(function (li) {
-			li.style.marginTop = "0rem";
-			// li.style.height = "20vh";
-		});
+		scroll.style.width = "570.16px";
+		portalLinks.style.fontSize = "1rem";
+		portalLinks.style.left = "80px";
+		portalLinks.style.top = "200px";
 	}
 }
 
